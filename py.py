291482f1fc15cf4a6c2b9a0244dd1405e1816c4c
Harshith-Nishan <harshithganiga21@gmail.com>
2026-08-05 12:57:26 +0530

    learning

--- py.py
+++ py.py
@@ -1,9 +1,21 @@
-# h="haibro" g
-# print(h[-5:-1])
+# # h="haibro" g
+# # print(h[-5:-1])
 
-list=[1,'gfg',6.6]
-lists=[1,'gfg',6.8]
-#print(list,lists)
-#list.append(lists)
-list.insert(3,66)
-print(list)
+# list=[1,'gfg',6.6]
+# lists=[1,'gfg',6.8]
+# #print(list,lists)
+# #list.append(lists)
+# list.insert(3,66)
+# print(list)
+# list.pop()
+# print(list)
+# list.append(9)
+# print(list)
+# del list[1:]
+# print(list)
+# list.extend(lists)
+# print(list)
+# list.extend([1,2,3])
+# print(list)
+# list.sort
+# print(list)
