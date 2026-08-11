21c6b0c4b0a8425f6c99446e5b32ac783e32ec5f
Harshith-Nishan <harshithganiga21@gmail.com>
2026-08-11 15:02:41 +0530

    Auto commit

--- py.py
+++ py.py
@@ -102,3 +102,23 @@
 
 z=input("enter the number")
 print(z)
+list=[1,'gfg',6.6]
+lists=[1,'gfg',6.8]
+#print(list,lists)
+#list.append(lists)
+list.insert(3,66)
+print(list)
+list.pop()
+print(list)
+list.append(9)
+print(list)
+del list[1:]
+print(list)
+list.extend(lists)
+print(list)
+list.extend([1,2,3])
+print(list)
+list.sort
+print(list)
+
+j
