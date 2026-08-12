b594cb153c6d0f18b890e003a43a82e25d5748db
Harshith-Nishan <harshithganiga21@gmail.com>
2026-08-12 11:27:18 +0530

    Update

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
+# import streamlit as st
+
+# st.title("My First Streamlit App")
+
+# st.write("Hello!")
+
 import streamlit as st
 
-st.title("My First Streamlit App")
+st.title("My First GUI")
 
-st.write("Hello!")
+name = st.text_input("Enter your name")
+
+if st.button("Submit"):
+    st.write("Hello", name)
